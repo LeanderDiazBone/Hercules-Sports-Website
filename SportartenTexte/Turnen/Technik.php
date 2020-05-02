59375d499d1de474d273389fa1561d36b1441ee2
LeanderGitHub <leanderdiazbone@gmail.com>
2020-05-02 18:44:27 +0200

    Videos eingefügt

---
+++ SportartenTexte/Turnen/Technik.php
@@ -0,0 +1,4 @@
+<h3>Bodenturnen</h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/6T2AkcZN0J8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<h3>Reck</h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZwxVqMPQDRo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
